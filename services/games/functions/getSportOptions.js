03690b324de6a25ca834ba628a016eb54cb313ee
Chris Burke <burke1791@gmail.com>
2020-08-31 12:20:54 -0500

    reorganized services folders

--- services/games/functions/getSportOptions.js
+++ services/games/functions/getSportOptions.js
@@ -1,14 +1,12 @@
-const connection = require('../../utilities/db').connection;
+const connection = require('../../../utilities/db').connection;
 const mssql = require('mssql');
 
-export async function getGamesBySportId(event, context, callback) {
+export async function getSportOptions(event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
 
   console.log(event);
 
   let cognitoSub = event.cognitoPoolClaims.sub;
-  let sportId = +event.path.sportId;
-  let dateString = event.path.date;
 
   try {
     if (!connection.isConnected) {
@@ -18,10 +16,8 @@ export async function getGamesBySportId(event, context, callback) {
     const request = new mssql.Request();
 
     request.input('CognitoSub', mssql.VarChar(256), cognitoSub);
-    request.input('SportId', mssql.Int(), sportId);
-    request.input('Date', mssql.VarChar(), dateString);
 
-    let result = await request.execute('dbo.up_AdminGetGamesBySportId');
+    let result = await request.execute('dbo.up_AdminGetActiveSports');
     console.log(result);
 
     callback(null, result.recordset);
