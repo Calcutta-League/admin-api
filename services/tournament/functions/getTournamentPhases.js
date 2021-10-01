c112c61c658b17b2bbf401348894e741cdf109af
Chris Burke <burke1791@gmail.com>
2021-10-01 06:39:48 -0500

    endpoints for the tournament page

---
+++ services/tournament/functions/getTournamentPhases.js
@@ -0,0 +1,28 @@
+const connection = require('../utilities/db').connection;
+const mssql = require('mssql');
+
+export async function getTournamentPhases(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  let cognitoSub = event.cognitoPoolClaims.sub;
+
+  let tournamentId = event.path.tournamentId;
+
+  try {
+    if (!connection.isConnected) {
+      await connection.createConnection();
+    }
+
+    const request = new mssql.Request();
+
+    request.input('CognitoSub', mssql.VarChar(256), cognitoSub);
+    request.input('TournamentId', mssql.SmallInt, tournamentId);
+
+    let result = await request.execute('dbo.up_AdminGetTournamentPhases');
+
+    callback(null, result.recordset);
+  } catch (error) {
+    console.log(error);
+    callback(null, error);
+  }
+}
