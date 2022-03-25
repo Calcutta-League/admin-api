25bcb407029c54940a5f12bee204f54dbba7a574
Chris Burke <burke1791@gmail.com>
2022-03-24 19:08:05 -0500

    MMC-60: added a new tournament phase endpoint

---
+++ common/tournamentPhaseType.js
@@ -0,0 +1,18 @@
+
+/**
+ * @function populateTournamentPhasesTVP
+ * @param {Array<String>} phases - a list of tournament phase names
+ * @returns a sql server table-valued parameter populated with the provided tournament phases
+ */
+export function populateTournamentPhasesTVP(phases) {
+  let tvp = new mssql.Table();
+
+  tvp.columns.add('PhaseNum', mssql.TinyInt);
+  tvp.columns.add('PhaseDescription', mssql.VarChar(100));
+
+  phases.forEach((phaseName, id) => {
+    tvp.rows.add(id, phaseName);
+  });
+
+  return tvp;
+}
