b1215720c4d6adc863b085693b0b2dbad788f6b2
Chris Burke <burke1791@gmail.com>
2024-03-10 12:03:19 -0500

    MMC-167: added endpoint to synchronize slots

---
+++ services/management/functions/synchronizeSlots.js
@@ -0,0 +1,72 @@
+import AWS from 'aws-sdk';
+import { LAMBDAS } from '../utilities/constants';
+
+const lambda = new AWS.Lambda();
+
+/**
+ * This lambda synchronizes the AuctionSlots in dynamodb every time a human admin
+ * updates a TournamentSlot in the admin portal
+ */
+
+export async function synchronizeSlots(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const cognitoSub = event.cognitoPoolClaims.sub;
+  const { tournamentRegimeId } = event.body;
+
+  try {
+    const leagues = await pullLeaguesForTournamentRegime(cognitoSub, tournamentRegimeId);
+    const leagueCount = leagues.length;
+
+    for (let l of leagues) {
+      const slots = await pullLeagueSlots(cognitoSub, l.LeagueId);
+
+      await setSlotsInDynamoDb(l.LeagueId, slots);
+    }
+
+    callback(null, { message: `${leagueCount} leagues updated`});
+  } catch (error) {
+    console.log(error);
+    callback(null, { error: error });
+  }
+}
+
+async function pullLeaguesForTournamentRegime(cognitoSub, tournamentRegimeId) {
+  const lambdaParams = {
+    FunctionName: LAMBDAS.PULL_LEAGUES_FOR_TOURNAMENT_REGIME,
+    LogType: 'Tail',
+    Payload: JSON.stringify({
+      cognitoSub: cognitoSub,
+      tournamentRegimeId: tournamentRegimeId
+    })
+  };
+
+  const lambdaResponse = await lambda.invoke(lambdaParams).promise();
+
+  return JSON.parse(lambdaResponse.Payload);
+}
+
+async function pullLeagueSlots(cognitoSub, leagueId) {
+  const lambdaParams = {
+    FunctionName: LAMBDAS.PULL_LEAGUES_FOR_TOURNAMENT_REGIME,
+    LogType: 'Tail',
+    Payload: JSON.stringify({
+      cognitoSub: cognitoSub,
+      tournamentRegimeId: tournamentRegimeId
+    })
+  };
+
+  const lambdaResponse = await lambda.invoke(lambdaParams).promise();
+
+  return JSON.parse(lambdaResponse.Payload);
+}
+
+async function setSlotsInDynamoDb(leagueId, slots) {
+  const lambdaParams = {
+    FunctionName: LAMBDAS.SYNC_AUCTION_SETTINGS,
+    LogType: 'Tail',
+    Payload: JSON.stringify({ leagueId: leagueId, settingCategory: 'SLOT', settings: slots })
+  };
+
+  await lambda.invoke(lambdaParams).promise();
+}
--- services/management/utilities/constants.js
+++ services/management/utilities/constants.js
@@ -2,7 +2,10 @@ export const LAMBDAS = {
   DUMP_DATA_INTO_DYNAMODB: `calcutta-admin-management-${process.env.APP_ENV}-dumpDataIntoDynamoDb`,
   PULL_DATA_FROM_SQL_SERVER: `calcutta-admin-management-${process.env.APP_ENV}-pullDataFromSqlServer`,
   SKIP_SYNC_INVOKABLE: `calcutta-admin-management-${process.env.APP_ENV}-skipSyncInvokable`,
-  GET_LEGACY_LEAGUES_INVOKABLE: `calcutta-admin-management-${process.env.APP_ENV}-getLegacyLeaguesInvokable`
+  GET_LEGACY_LEAGUES_INVOKABLE: `calcutta-admin-management-${process.env.APP_ENV}-getLegacyLeaguesInvokable`,
+  PULL_LEAGUES_FOR_TOURNAMENT_REGIME: `calcutta-admin-management-${process.env.APP_ENV}-pullLeaguesForTournamentRegime`,
+  PULL_LEAGUE_SLOTS: `calcutta-admin-management-${process.env.APP_ENV}-pullLeagueSlots`,
+  SYNC_AUCTION_SETTINGS: `calcutta-auction-service-v2-${process.env.APP_ENV}-syncAuctionSettings`
 };
 
 export const DYNAMODB_TABLES = {
